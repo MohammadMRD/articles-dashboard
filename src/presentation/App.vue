e540b20c16dace2783e668d067ed9d026a7744ef
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-02 06:27:22 +0330

    Install and config Bootstrap

--- src/presentation/App.vue
+++ src/presentation/App.vue
@@ -5,6 +5,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+// Main Styles
+import '@/presentation/assets/styles/main.scss'
+
+// Component
 export default defineComponent({
   name: 'App',
 })
