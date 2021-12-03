1622bf55819146ae82498d1e01b9f3af5c665da4
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-04 00:53:18 +0330

    Add dashboard-layout component

---
+++ src/presentation/layouts/dashboard-layout.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="dashboard">
+    <!-- Header -->
+    <dashboard-header class="dashboard__header"></dashboard-header>
+
+    <!-- Sidebar -->
+    <dashboard-sidebar class="dashboard__sidebar"></dashboard-sidebar>
+
+    <!-- Main -->
+    <main class="dashboard__main container mt-4">
+      <slot></slot>
+    </main>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import DashboardHeader from '@/presentation/components/dashboard-header.vue'
+import DashboardSidebar from '@/presentation/components/dashboard-sidebar.vue'
+
+export default defineComponent({
+  name: 'dashboard-layout',
+
+  components: {
+    DashboardHeader,
+    DashboardSidebar,
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.dashboard {
+  display: grid;
+  position: relative;
+  height: 100vh;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    'header header'
+    'sidebar main';
+
+  &__header {
+    grid-area: header;
+  }
+
+  &__sidebar {
+    grid-area: sidebar;
+    position: relative;
+  }
+
+  &__main {
+    grid-area: main;
+    overflow-y: auto;
+  }
+}
+</style>
