26d8f5684e48e87a98e4c280137b59f49e11d028
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-03 17:24:08 +0330

    Add ad-page component The ad-page component manages the layout of pages

---
+++ src/presentation/components/ad-page.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="pageLayout">
+    <template v-for="(slot, name) in $slots" :key="name" v-slot:[name]>
+      <component :is="slot"></component>
+    </template>
+  </component>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import DefaultLayout from '@/presentation/layouts/default-layout.vue'
+
+export default defineComponent({
+  name: 'auth-view',
+
+  computed: {
+    pageLayout() {
+      return this.$route.meta.layout || DefaultLayout
+    },
+  },
+})
+</script>
