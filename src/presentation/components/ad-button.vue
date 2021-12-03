92420b9a3db9bde175bf552be0890aacde8bda09
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-03 17:55:42 +0330

    Add ad-button component

---
+++ src/presentation/components/ad-button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button class="btn" :class="variantClass">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export enum ButtonVariants {
+  primary = 'primary',
+  secondary = 'secondary',
+  success = 'success',
+  danger = 'danger',
+  warning = 'warning',
+  info = 'info',
+  light = 'light',
+  dark = 'dark',
+}
+
+export default defineComponent({
+  name: 'ad-button',
+
+  props: {
+    variant: {
+      type: String as PropType<ButtonVariants>,
+      required: true,
+      default: ButtonVariants.primary,
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    variantClass() {
+      return `btn${this.outline ? '-outline' : ''}-${this.variant}`
+    },
+  },
+})
+
+// TODO: Move Bootstrap's button styles to this file to isolate button component
+</script>
