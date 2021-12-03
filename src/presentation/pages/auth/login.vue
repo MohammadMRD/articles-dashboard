81be55bb4cb671744f3b08ba6e8a2d45fad0369c
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-03 19:18:57 +0330

    Add the login page without functionality

---
+++ src/presentation/pages/auth/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <ad-page>
+    <!-- Title -->
+    <template #header>Login</template>
+
+    <!-- Form -->
+    <ad-input id="login-email" label="Email" type="email" />
+    <ad-input id="login-password" label="Password" type="password" />
+    <ad-button variant="primary" class="mt-3" type="submit"> Login </ad-button>
+
+    <!-- Footer -->
+    <template #footer>
+      <span>Don't have a account?</span>
+      <router-link to="" class="text-decoration-none text-dark mx-2">
+        <strong>Register Now</strong>
+      </router-link>
+    </template>
+  </ad-page>
+</template>
--- src/presentation/router/routes.ts
+++ src/presentation/router/routes.ts
@@ -1,11 +1,29 @@
 import { RouteRecordRaw } from 'vue-router'
 import Home from '@/presentation/pages/Home.vue'
+import AuthLayout from '@/presentation/layouts/auth-layout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+
+  // Auth pages
+  {
+    path: '/auth',
+    redirect: { name: 'login' },
+    component: () => import('@/presentation/pages/_default.vue'),
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import('@/presentation/pages/auth/login.vue'),
+        meta: {
+          layout: AuthLayout,
+        },
+      },
+    ],
   },
 ]
 
