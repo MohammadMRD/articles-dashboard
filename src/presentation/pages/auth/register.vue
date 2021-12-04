23d11233252e099a9e13b8f123269ba9d256f1b4
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-04 22:22:46 +0330

    Connect Register page to the API

--- src/presentation/pages/auth/register.vue
+++ src/presentation/pages/auth/register.vue
@@ -4,10 +4,10 @@
     <template #header>Register</template>
 
     <!-- Form -->
-    <ad-input id="register-user" label="User" type="text" />
-    <ad-input id="register-email" label="Email" type="email" />
-    <ad-input id="register-password" label="Password" type="password" />
-    <ad-button variant="primary" class="mt-3" type="submit"> Register </ad-button>
+    <ad-input v-model="registerDTO.username" id="register-user" label="User" type="text" />
+    <ad-input v-model="registerDTO.email" id="register-email" label="Email" type="email" />
+    <ad-input v-model="registerDTO.password" id="register-password" label="Password" type="password" />
+    <ad-button variant="primary" class="mt-3" type="submit" @click.prevent="handleRegister"> Register </ad-button>
 
     <!-- Footer -->
     <template #footer>
@@ -18,3 +18,28 @@
     </template>
   </ad-page>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import { RegisterDTO } from '@/core'
+
+export default defineComponent({
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const registerDTO = reactive(new RegisterDTO('', '', ''))
+
+    async function handleRegister() {
+      // TODO: Add validation
+      await store.dispatch('userModule/register', registerDTO)
+
+      // TODO: Define enum for routes' name
+      router.push({ name: 'articles' })
+    }
+
+    return { registerDTO, handleRegister }
+  },
+})
+</script>
