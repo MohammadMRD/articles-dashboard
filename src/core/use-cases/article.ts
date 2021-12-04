61e83cdde41e55e1207e0f9f4c2187a4074a65f1
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-04 15:23:16 +0330

    Add use case of `Get an article`

--- src/core/use-cases/article.ts
+++ src/core/use-cases/article.ts
@@ -1,10 +1,22 @@
-import { IArticleRepository, PageNumber } from '@/core/use-cases/interfaces'
-import { Article } from '../entities'
+import {
+  IArticleRepository,
+  PageNumber,
+  IArticleQuery,
+} from '@/core/use-cases/interfaces'
+import { Article, Slug } from '../entities'
 
 export class GetAllArticlesUseCase {
   constructor(private articleRepository: IArticleRepository) {}
 
   execute(pageNumber?: PageNumber): Promise<Article[]> {
     return this.articleRepository.list(pageNumber)
+  }
+}
+
+export class GetArticleUseCase {
+  constructor(private articleRepository: IArticleRepository) {}
+
+  execute(query: IArticleQuery): Promise<Article> {
+    return this.articleRepository.get(query)
   }
 }
--- src/core/use-cases/interfaces/article-repository-interface.ts
+++ src/core/use-cases/interfaces/article-repository-interface.ts
@@ -1,7 +1,11 @@
-import type { Article } from '@/core/entities'
+import type { Article, Slug } from '@/core/entities'
 
 export type PageNumber = number // Start with 1
+export interface IArticleQuery {
+  slug: Slug
+}
 
 export interface IArticleRepository {
   list(page?: PageNumber): Promise<Article[]>
+  get(query: IArticleQuery): Promise<Article>
 }
