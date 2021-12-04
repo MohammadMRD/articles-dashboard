c5e41d36db5e02952d13e7f4d2008e12d500ffef
Mohammad Moradi <mohammadmrd@hotmail.com>
2021-12-04 19:40:04 +0330

    Add API service for handling http requests

---
+++ src/core/services/api.ts
@@ -0,0 +1,60 @@
+import axios, { Method, Axios, AxiosResponse } from 'axios'
+
+export type ApiURL = string
+export type ApiBaseURL = string
+export type ApiHeaders = { [key: string]: string }
+export type ApiResponseHeaders = Record<string, string> & {
+  'set-cookie'?: string[]
+}
+
+export interface IApiResponse<D = unknown> {
+  status: number
+  headers: ApiResponseHeaders
+  data: D
+}
+
+export interface IApiOptions<D = unknown> {
+  baseURL?: ApiBaseURL
+  url?: string
+  method?: Method
+  headers?: ApiHeaders
+  data?: D
+  params?: never
+  withAuth?: boolean
+  token?: string
+}
+
+export class Api {
+  private axios: Axios
+  public static sharedToken: string
+
+  constructor(options?: IApiOptions) {
+    this.axios = axios.create(options)
+    this.axios.interceptors.response.use(this.normalizeResponse)
+  }
+
+  private normalizeResponse(response: AxiosResponse): IApiResponse {
+    const { data, headers, status } = response
+
+    return { data, headers, status }
+  }
+
+  send<R = unknown>(options: IApiOptions | string): Promise<IApiResponse<R>> {
+    if (typeof options === 'string') options = { url: options, method: 'GET' }
+
+    options.headers = {
+      'X-Requested-With': 'XMLHttpRequest',
+      ...(options.headers || {}),
+    }
+
+    const { withAuth, token, ...apiOptions } = options
+
+    if (withAuth) {
+      apiOptions.headers = apiOptions.headers ?? {}
+      apiOptions.headers.Authorization = `Token ${token || Api.sharedToken}`
+    }
+
+    // TODO: Normalize Errors with assertion service
+    return this.axios.request(apiOptions)
+  }
+}
